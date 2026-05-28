f15cfaf99d697cc0b60a34d3d291f62bec9d248c
John <john.ocamposabg@gmail.com>
2026-05-28 19:20:04 -0300

    estructura de carpetas y conexion con base de datos

---
+++ backend/database/connection.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+load_dotenv()
+engine = create_engine(os.getenv("DATABASE_URL"))
+Base = declarative_base()
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
