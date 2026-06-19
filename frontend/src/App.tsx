02902ceb062b6769866bbcc191711a806e55e3cb
John <john.ocamposabg@gmail.com>
2026-06-18 23:28:02 -0300

    Redirección de ruta raíz

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom'
 import DocumentsPage from './pages/DocumentsPage'
 import ChatPage from './pages/ChatPage'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/documents" replace />} />
         <Route path="/documents" element={<DocumentsPage />} />
         <Route path="/documents/:id" element={<ChatPage />} />
       </Routes>
